bdb8265f25f5451fe3cde690701590239a9faf3b
Iclipsee <iclipskii@gmail.com>
2020-03-27 22:53:26 +0100

    Updated and started BARO

--- src/main/java/de/Iclipse/BARO/BARO.java
+++ src/main/java/de/Iclipse/BARO/BARO.java
@@ -2,10 +2,15 @@ package de.Iclipse.BARO;
 
 import com.google.common.base.Joiner;
 import org.bukkit.Bukkit;
+import org.bukkit.WorldCreator;
 import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,6 +29,7 @@ public class BARO extends JavaPlugin{
         registerListener();
         registerCommands();
         createTables();
+        loadMap();
     }
 
     @Override
@@ -39,6 +45,32 @@ public class BARO extends JavaPlugin{
 
     public void createTables(){
     }
+
+    public void loadMap(){
+        File existing = new File("/home/IMNetzwerk/Welten/BAROMap");
+        File toCreate = new File(Data.instance.getDataFolder().getAbsoluteFile().getParentFile().getParentFile().getAbsolutePath().getBytes() + "/world");
+        try {
+            copyFilesInDirectory(existing, toCreate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        getServer().createWorld(new WorldCreator("BAROMap"));
+    }
+
+    private static void copyFilesInDirectory(File from, File to) throws IOException {
+        if(!to.exists()) {
+            to.mkdirs();
+        }
+        for (File file : from.listFiles()) {
+            if (file.isDirectory()) {
+                copyFilesInDirectory(file, new File(to.getAbsolutePath() + "/" + file.getName()));
+            } else {
+                File n = new File(to.getAbsolutePath() + "/" + file.getName());
+                Files.copy(file.toPath(), n.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+    }
+
 
 
 }
